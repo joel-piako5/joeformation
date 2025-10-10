3b4220e7d90353e08ebac6c07c9eb11a0fb63dde
joel-piako5 <joefroid3@gmail.com>
2025-10-10 14:52:34 +0200

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,9 +1,1 @@
-# init_db.py
-from app import app, db
 
-# Ce script sert uniquement à créer les tables dans la base de données PostgreSQL.
-# Il faut l’exécuter une seule fois sur Render (via le shell).
-
-with app.app_context():
-    db.create_all()
-    print("✅ Base de données initialisée avec succès sur PostgreSQL !")

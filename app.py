611bd1ac80850a3b4df48b6ee11bae9766125487
joel-piako5 <joefroid3@gmail.com>
2025-10-15 07:28:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -172,7 +172,7 @@ def resultats():
 def liste_etudiants():
     if "user_id" not in session:
         return redirect(url_for("logi"))
-    etudiants = User.query.all()
+    users = User.query.all()
     return render_template("liste_etudiants.html", etudiants=etudiants)
 
 # ------------------ ADMIN ------------------
@@ -348,6 +348,7 @@ if __name__ == "__main__":
         db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
 
 
